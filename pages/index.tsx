56abb4a5a6b0d5d914d6c32b2465cfab6af79f23
Damian Gianni <damiangianni@entregar.local>
2019-06-21 14:09:04 -0300

    For responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,32 @@
 // import Link from "next/link";
-import Splash from "../components/splash";
+import styled, { css, keyframes } from "styled-components";
+import Splash from "../components/ui/splash";
 import { AppBar, Toolbar, CssBaseline } from "@material-ui/core";
 
-export default () => (
-  <div>
-    <CssBaseline />
-    <AppBar position="static">
-      <Toolbar>Estudiantes 2020</Toolbar>
-    </AppBar>
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <img
-        src="https://estudiantes2020.sfo2.digitaloceanspaces.com/logo-app.png"
-        style={{ width: "50%" }}
-      />
-      <div>
-        <h2>PROXIMAMENTE...</h2>
+export default () => {
+  const ImgCommingSoon = styled.img`
+    width: 50%;
+    @media (min-width: 1024px) {
+      width: 20%;
+    }
+  `;
+
+  return (
+    <div>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>Estudiantes 2020</Toolbar>
+      </AppBar>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <ImgCommingSoon src="https://estudiantes2020.sfo2.digitaloceanspaces.com/logo-app.png" />
+        <div>
+          <h2>PROXIMAMENTE...</h2>
+        </div>
       </div>
-    </div>
-    {/* <Link href="/about">
+      {/* <Link href="/about">
       <span>about</span>
     </Link> */}
-    <Splash />
-  </div>
-);
+      <Splash />
+    </div>
+  );
+};
