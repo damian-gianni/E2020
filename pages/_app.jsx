46947a9fc5cb8e889853145a0ac84616ce27781a
Damian Gianni <damiangianni@MackBook-Damian-Gianni.local>
2019-07-11 13:55:58 -0300

    Register & unregister SW

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,35 +1,40 @@
 import React from "react";
 import App, { Container } from "next/app";
+import { register, unregister } from "next-offline/runtime";
 import Head from "next/head";
 import { ThemeProvider } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../theme";
 
 class MyApp extends App {
-    componentDidMount() {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector("#jss-server-side");
-        if (jssStyles) {
-            jssStyles.parentNode && jssStyles.parentNode.removeChild(jssStyles);
-        }
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentNode && jssStyles.parentNode.removeChild(jssStyles);
     }
+    register();
+  }
 
-    render() {
-        const { Component, pageProps } = this.props;
-        return (
-            <Container>
-                <Head>
-                    <title>Estudiantes 2020 || Home</title>
-                </Head>
-                <ThemeProvider theme={theme}>
-                    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                    <CssBaseline />
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </Container>
-        );
-    }
+  componentWillUnmount() {
+    unregister();
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Head>
+          <title>Estudiantes 2020 || Home</title>
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Container>
+    );
+  }
 }
 
 export default MyApp;
-
