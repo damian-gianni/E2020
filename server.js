92888da23b3f31f2ede1dd84a9c2fe4db3c2225f
Damian Gianni <damiangianni@MackBook-Damian-Gianni.local>
2019-07-11 10:36:02 -0300

    Fixed HTTPS

--- server.js
+++ server.js
@@ -15,62 +15,72 @@ app
   .prepare()
   .then(() => {
     if (HTTPS) {
-      const options = {
-        key: fs.readFileSync(
-          "/etc/letsencrypt/live/estudiantes2020.com.ar/privkey.pem"
-        ),
-        cert: fs.readFileSync(
-          "/etc/letsencrypt/live/estudiantes2020.com.ar/fullchain.pem"
-        )
-      };
+      let serviceKey, certificate;
+      // Development
+      if (dev) {
+        pem.createCertificate({ days: 1, selfSigned: true }, function(
+          err,
+          keys
+        ) {
+          https
+            .createServer(
+              {
+                key: keys.serviceKey,
+                cert: keys.certificate
+              },
+              (req, res) => {
+                const parsedUrl = parse(req.url, true);
+                const { pathname } = parsedUrl;
 
-      https
-        .createServer(options, (req, res) => {
-          // res.writeHead(200);
-          const parsedUrl = parse(req.url, true);
-          const { pathname } = parsedUrl;
+                // handle GET request to /service-worker.js
+                if (pathname === "/service-worker.js") {
+                  const filePath = join(__dirname, ".next", pathname);
 
-          // handle GET request to /service-worker.js
-          if (pathname === "/service-worker.js") {
-            const filePath = join(__dirname, ".next", pathname);
-
-            app.serveStatic(req, res, filePath);
-          } else {
-            handle(req, res, parsedUrl);
-          }
-          // res.end("Initialize with HTTPS\n");
-        })
-        .listen(PORT, err => {
-          if (err) throw err;
-          console.log(`> Ready on https://localhost:${PORT}`);
+                  app.serveStatic(req, res, filePath);
+                } else {
+                  handle(req, res, parsedUrl);
+                }
+              }
+            )
+            .listen(PORT, err => {
+              if (err) throw err;
+              console.log(`> Ready on https://localhost:${PORT}`);
+            });
         });
-      //  pem.createCertificate({ days: 1, selfSigned: true }, function(err, keys) {
-      //   https
-      //     .createServer(
-      //       {
-      //         key: keys.serviceKey,
-      //         cert: keys.certificate
-      //       },
-      //       (req, res) => {
-      //         const parsedUrl = parse(req.url, true);
-      //         const { pathname } = parsedUrl;
+      } else {
+        // Production
+        serviceKey = fs.readFileSync(
+          "/etc/letsencrypt/live/estudiantes2020.com.ar/privkey.pem"
+        );
+        certificate = fs.readFileSync(
+          "/etc/letsencrypt/live/estudiantes2020.com.ar/fullchain.pem"
+        );
+        const options = {
+          key: serviceKey,
+          cert: certificate
+        };
 
-      //         // handle GET request to /service-worker.js
-      //         if (pathname === "/service-worker.js") {
-      //           const filePath = join(__dirname, ".next", pathname);
+        https
+          .createServer(options, (req, res) => {
+            const parsedUrl = parse(req.url, true);
+            const { pathname } = parsedUrl;
 
-      //           app.serveStatic(req, res, filePath);
-      //         } else {
-      //           handle(req, res, parsedUrl);
-      //         }
-      //       }
-      //     )
-      //     .listen(PORT, err => {
-      //       if (err) throw err;
-      //       console.log(`> Ready on https://localhost:${PORT}`);
-      //     });
-      // });
+            // handle GET request to /service-worker.js
+            if (pathname === "/service-worker.js") {
+              const filePath = join(__dirname, ".next", pathname);
+
+              app.serveStatic(req, res, filePath);
+            } else {
+              handle(req, res, parsedUrl);
+            }
+          })
+          .listen(PORT, err => {
+            if (err) throw err;
+            console.log(`> Ready on https://localhost:${PORT}`);
+          });
+      }
     } else {
+      //HTTP
       createServer((req, res) => {
         const parsedUrl = parse(req.url, true);
         const { pathname } = parsedUrl;
